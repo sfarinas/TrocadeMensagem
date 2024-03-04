5bdd968c6ff06af40251aff238c4b5ee27a6e126
Salomao Farina <sfarinas@gmail.com>
2024-03-03 22:04:33 -0300

    Update CalcularIMC.java
    Finalizado a classe CalcularIMC

--- app/src/main/java/com/example/trocademensagem/CalculaIMC.java
+++ app/src/main/java/com/example/trocademensagem/CalculaIMC.java
@@ -2,10 +2,12 @@ package com.example.trocademensagem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class CalculaIMC extends AppCompatActivity {
@@ -18,6 +20,26 @@ public class CalculaIMC extends AppCompatActivity {
         editTextNome = findViewById(R.id.editTextNome);
         editTextPeso = findViewById(R.id.editTextPeso);
         editTextAltura = findViewById(R.id.editTextAltura);
+
+        String nome = getIntent().getExtras().getString("nome");
+        editTextNome.setText(nome);
+    }
+
+    public void calcularVoltar(View view){
+        double peso, altura, imc;
+
+        peso = Double.parseDouble(editTextPeso.getText().toString());
+        altura = Double.parseDouble(editTextAltura.getText().toString());
+
+        imc = peso / Math.pow(altura,2);
+        Toast.makeText(getApplicationContext(), "IMC ="+String.valueOf(imc),
+                Toast.LENGTH_LONG).show();
+
+        Intent returnIntent = new Intent();
+
+        returnIntent.putExtra("imc", imc);
+        setResult(RESULT_OK,returnIntent);
+        finish();
     }
 }
 
